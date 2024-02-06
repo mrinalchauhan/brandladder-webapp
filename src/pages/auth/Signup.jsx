631a616acf322b93ca42232f5e5c138883ca7354
anuragkmr45 <anuragkmr825@gmail.com>
2024-02-06 21:33:44 +0530

    integrate chatbot

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -8,11 +8,11 @@ const LoginPage = () => {
                 <form className="space-y-4">
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                        <input className="input-ghost-primary input" placeholder="Email" type='email' />
+                        <input className="input-ghost-primary input max-w-full" placeholder="Email" type='email' />
                     </div>
                     <div>
                         <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                        <input className="input-ghost-primary input" placeholder="Password" type='password' />
+                        <input className="input-ghost-primary input max-w-full" placeholder="Password" type='password' />
                     </div>
                     <div>
                         <button type="submit" className="w-full bg-orange-6 text-white py-2 px-4 rounded-md hover:bg-orange-7">Login</button>
