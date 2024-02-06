8abb25811b813615bb537017d9aa01fe10e212ac
anuragkmr45 <anuragkmr825@gmail.com>
2024-02-06 21:04:47 +0530

    temp remove firebase

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import { FirestoreProvider } from './context/FirestoreContext'
+// import { AuthProvider } from './context/AuthContext'
+// import { FirestoreProvider } from './context/FirestoreContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <FirestoreProvider>
-          <App />
-        </FirestoreProvider>
-      </AuthProvider>
+      {/* <AuthProvider>
+        <FirestoreProvider> */}
+      <App />
+      {/* </FirestoreProvider>
+      </AuthProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
