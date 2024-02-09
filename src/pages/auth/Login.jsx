8e9319705490ac0eec0235b7c11a703e45221c1d
anuragkmr45 <anuragkmr825@gmail.com>
2024-02-10 00:12:12 +0530

    typo error

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -97,7 +97,7 @@ const LoginPage = () => {
                         <button
                             onClick={handleEmailLogin}
                             className="btn btn-outline bg-inherit border-orange-6 text-black shadow-md transition-all ease-in-out duration-500 hover:bg-orange-6 hover:text-orange-2 hover:shadow-2xl hover:border-orange-6 w-full">
-                            Login
+                            Signin
                         </button>
                     </div>
                 </div>
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -171,7 +171,7 @@ const SignupPage = () => {
                         <button
                             onClick={handleEmailSignin}
                             className="btn btn-outline bg-inherit border-orange-6 text-black shadow-md transition-all ease-in-out duration-500 hover:bg-orange-6 hover:text-orange-2 hover:shadow-2xl hover:border-orange-6 w-full">
-                            SignIn
+                            Signup
                         </button>
                     </div>
                 </div>
